83c037323f1ac3cdc6c699211186827874882665
FiReLoRd44 <mehtasiddha@gmail.com>
2022-02-28 11:52:25 +0530

    Added some comments in the Arduino Code

--- check_light_ldr/check_light_ldr.ino
+++ check_light_ldr/check_light_ldr.ino
@@ -2,6 +2,7 @@
 int light;
 void setup() {
   // put your setup code here, to run once:
+  //serial transmission begining on 9600 baud rate
   Serial.begin(9600);
 }
 
@@ -9,6 +10,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   //reading the analog value from pin A0
   light = analogRead(A0);
+  //printing the analog values on the serial monitor
   Serial.println(light);
   delay(100);
 }
